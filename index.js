b27dd580fd80d3e2c65caf10cd6d7bb8f5bc3de1
shokirzhonovkamoliddin-max <shokirzhonovkamoliddin@gmail.com>
2026-05-16 16:38:58 +0500

    done

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const api = `https://www.omdbapi.com/?s=terminator&apikey=c65fcde9`;
+const api = `https://www.omdbapi.com/?apikey=c65fcde9&s=terminator`;
+
 const elloading = document.querySelector(".loading");
 const ellist = document.querySelector(".cards");
+const elform = document.querySelector(".form");
+const elsearch = document.querySelector(".search");
 
 const getdata = (url) => {
-    elloading.innerHTML = "Loading...";
-  fetch(url)    
+  elloading.innerHTML = "Loading...";
+  ellist.innerHTML = "";
+
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
       showdata(data);
     })
     .finally(() => {
-        elloading.innerHTML = "";
-    })
+      elloading.innerHTML = "";
+    });
 };
 
 getdata(api);
+
 function showdata(movies) {
   if (movies.Response === "True") {
     const { Search } = movies;
@@ -23,15 +29,25 @@ function showdata(movies) {
       const { Title, Poster, Year, Type } = element;
 
       ellist.innerHTML += `
-            <div class="card">
-              <img src="${Poster}" alt="" />
-              <h2>${Title}</h2>
-              <p>${Year}</p>
-              <p>${Type}</p>
-            </div>
-            `;
+        <div class="card">
+          <img src="${Poster}" alt="" />
+          <h2>${Title}</h2>
+          <p>${Year}</p>
+          <p>${Type}</p>
+        </div>
+      `;
     });
   } else {
-    console.log("not found");
+    ellist.innerHTML = `<h2>Movie not found</h2>`;
   }
 }
+
+elform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchValue = elsearch.value.trim();
+
+  if (searchValue !== "") {
+    getdata(`https://www.omdbapi.com/?apikey=c65fcde9&s=${searchValue}`);
+  }
+});
