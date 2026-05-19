3e5bd3540f05d75dd7ffe1c92c39359e699cdc1e
shokirzhonovkamoliddin-max <shokirzhonovkamoliddin@gmail.com>
2026-05-19 17:17:12 +0500

    finish

--- index.js
+++ index.js
@@ -1,53 +1,138 @@
-const api = `https://www.omdbapi.com/?apikey=c65fcde9&s=terminator`;
+const api = `https://www.omdbapi.com/?s=panda&apikey=c65fcde9`;
 
-const elloading = document.querySelector(".loading");
-const ellist = document.querySelector(".cards");
-const elform = document.querySelector(".form");
-const elsearch = document.querySelector(".search");
+const elLoading = document.querySelector(".loading");
+const elList = document.querySelector(".cards");
 
-const getdata = (url) => {
-  elloading.innerHTML = "Loading...";
-  ellist.innerHTML = "";
+const elForm = document.querySelector(".form");
+const elSearch = document.querySelector(".search");
+
+const details = document.querySelector(".details");
+
+const getData = (url) => {
+  elLoading.innerHTML = "Loading...";
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => {
-      showdata(data);
-    })
+    .then((data) => showData(data))
     .finally(() => {
-      elloading.innerHTML = "";
+      elLoading.innerHTML = "";
     });
 };
 
-getdata(api);
+getData(api);
 
-function showdata(movies) {
+elForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const query = elSearch.value.trim();
+
+  if (query !== "") {
+    const newApi = `https://www.omdbapi.com/?s=${query}&apikey=c65fcde9`;
+
+    getData(newApi);
+  }
+});
+
+function showData(movies) {
+  elList.innerHTML = "";
+
   if (movies.Response === "True") {
+
     const { Search } = movies;
 
     Search.forEach((element) => {
-      const { Title, Poster, Year, Type } = element;
 
-      ellist.innerHTML += `
+      const {
+        Poster,
+        Title,
+        Type,
+        Year,
+        imdbID
+      } = element;
+
+      elList.innerHTML += `
         <div class="card">
-          <img src="${Poster}" alt="" />
-          <h2>${Title}</h2>
-          <p>${Year}</p>
-          <p>${Type}</p>
+
+          <img class="poster" src="${Poster}" alt="" />
+
+          <h2 class="title">${Title}</h2>
+
+          <p class="year">${Year}</p>
+
+          <p class="type">${Type}</p>
+
+          <button
+            class="s-btn"
+            onclick="getOneMovies('${imdbID}')"
+          >
+            More Info
+          </button>
+
         </div>
       `;
     });
+
   } else {
-    ellist.innerHTML = `<h2>Movie not found</h2>`;
+    elList.innerHTML = `
+      <h2>Movie not found</h2>
+    `;
   }
 }
 
-elform.addEventListener("submit", (e) => {
-  e.preventDefault();
+function getOneMovies(id) {
 
-  const searchValue = elsearch.value.trim();
+  fetch(`https://www.omdbapi.com/?i=${id}&apikey=c65fcde9`)
+    .then((response) => response.json())
+    .then((data) => {
 
-  if (searchValue !== "") {
-    getdata(`https://www.omdbapi.com/?apikey=c65fcde9&s=${searchValue}`);
-  }
-});
+      const {
+        Title,
+        Year,
+        Poster,
+        Director,
+        Runtime,
+        Released,
+        Genre,
+        Rated,
+        Writer,
+        Actors,
+        Plot,
+      } = data;
+
+      details.innerHTML = `
+        <div class="more-info">
+
+          <div class="more-info-content">
+
+            <img class="modal-img" src="${Poster}" alt="" />
+
+            <div class="info">
+
+              <h1>${Title}</h1>
+
+              <p><span>Year:</span> ${Year}</p>
+
+              <p><span>Rated:</span> ${Rated}</p>
+
+              <p><span>Released:</span> ${Released}</p>
+
+              <p><span>Runtime:</span> ${Runtime}</p>
+
+              <p><span>Genre:</span> ${Genre}</p>
+
+              <p><span>Director:</span> ${Director}</p>
+
+              <p><span>Writer:</span> ${Writer}</p>
+
+              <p><span>Actors:</span> ${Actors}</p>
+
+              <p><span>Plot:</span> ${Plot}</p>
+
+            </div>
+
+          </div>
+
+        </div>
+      `;
+    });
+}
